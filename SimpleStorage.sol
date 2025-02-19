8ddf4a961f301115156cde8e0f6b8bbe1cd55ce4
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-02-19 15:13:40 +0530

    Dynamic arrays vs static arrays and Arrays for struct

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -9,16 +9,17 @@ pragma solidity  0.8.19; //version information
 // contract
 contract SimpleStorage {
     // arrays and struct
+    
+    // Dynamic array - length is dynamic
     uint256[] arrayData;
-    // struct : personalize our custom data type
+    // Stateic array - length = 6
+    uint256[6] arrayDataStatic;
+
     struct Person{      // type : Person
         // empty structs are not allowed
         uint256 age;
         string name;
     }
-    // creating a Person variable
-    Person public Collegue = Person(30,"Ava");
-    // or
-    Person public Collegue2 = Person({age:22,name:"John"});
-    
+    // defining arrays for struct
+    Person [] public listOfCollegues;
 }
