56f39e8ab33721ff4f1198f1c034593d137cbb6e
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-02-21 21:00:21 +0530

    mappings

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,28 +5,19 @@ pragma solidity  0.8.19; //version information
 // (>=0.8.18,<0.9.0) means range
 
 // SPDX-License-Identifier : MIT
-
-// contract
 contract SimpleStorage {
-
-    // arrays and struct
     struct Person{
         string name;
         uint256 age;
     }
-
-    // defining arrays for struct
     Person [] public listOfCollegues;
-
-    // Method - 1:
-    function addPerson(string memory name , uint256 age) public {
-        Person memory Friend = Person(name,age);
-        // push the created person to the list
-        listOfCollegues.push(Friend);   // items will be added from the 0 index
-    }
-
-    // Method - 2:
-    function addPersonType2(string memory name , uint256 age) public {
-        listOfCollegues.push(Person(name,age));
+    // careating a mapping function from name to age 
+    // default value of a mapping is 0
+    mapping (string => uint256) public nameMapsAge;
+    // function---
+    function addPerson(string calldata _name , uint256 _age) public {
+        listOfCollegues.push(Person(_name,_age));   // items will be added from the 0 index
+        // adding mapping functionality by name to age
+        nameMapsAge[_name] = _age;
     }
 }
