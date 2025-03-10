dce56907c08a8bb3ea12dc5535ab017d35c0d0e4
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-03-10 15:53:48 +0000

    FundMe basic idea

---
+++ FundMe/FundMe.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+contract FundMe {
+    // for funding
+    function fund() public{}
+    // for withdraawing
+    function withdraw() public {}
+}
