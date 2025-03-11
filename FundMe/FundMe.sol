c5e7f2165988dd48d4a88a53f9e6a97b30f92d37
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-03-11 18:36:12 +0530

    revert in fund me

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.19;
 contract FundMe {
     // for funding
+    uint256 public myValue   = 1;
     function fund() public payable {
+        // Here if the value send is less than 1ETH myValue remains as 1 but gas will still be spent since its executing the myValue and fund 
+        myValue = myValue+1;
         require(msg.value > 1e18,"Didnot send enough tokens");
     }
     // for withdraawing
