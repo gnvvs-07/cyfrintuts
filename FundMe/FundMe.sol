1f5f21cfb0bd518f41956166f8639f9b65e03246
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-03-11 18:29:01 +0530

    fund me function updated

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.19;
 contract FundMe {
     // for funding
-    function fund() public{}
+    function fund() public payable {
+        require(msg.value > 1e18,"Didnot send enough tokens");
+    }
     // for withdraawing
     function withdraw() public {}
 }
