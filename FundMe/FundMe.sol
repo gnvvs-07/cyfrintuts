1a3a24015846e1a2d36381a03d76552351b647fb
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-03-12 18:09:33 +0530

    solidity maths

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,30 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    function fund() public payable {
-        require(msg.value > 1e18,"Didnot send enough tokens");
+
+    uint256 public minimumUsd = 5e18;
+
+    AggregatorV3Interface private immutable priceFeed;
+    
+    // creating a construcotor for price feed for saving gas
+
+    constructor(){
+        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
-    // for withdraawing
-    function withdraw() public {}
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumUsd,"Didnot send enough ETH");
+    }
+
     function getPrice() public view returns (uint256){
         // address and ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price*1e18);
+        // Convert price from 8 to 18 decimal places
+        return uint256(price*1e10);
         
     }
-}
 
-//Recently changed  Data Feeds not include in the code 
- function getChainlinkDataFeedLatestAnswer() public view returns (int) {
-        // prettier-ignore
-        (
-            /* uint80 roundID */,
-            int answer,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = dataFeed.latestRoundData();
-        return answer;
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        // converting to usd
+        uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18;
+        return ethAmountInUsd;
     }
+}
