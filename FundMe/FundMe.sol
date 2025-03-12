ffe09f1bbff0c40392414146c183f20f370cd5fa
G.N.V.VinaySankar <vinaysankargokavarapu@gmail.com>
2025-03-12 19:23:29 +0530

    Price Converter library is being used

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
 
@@ -8,32 +9,11 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    AggregatorV3Interface private immutable priceFeed;
+    using PriceConverter for uint256;
     
-    // creating a construcotor for price feed for saving gas
-
-    constructor(){
-        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    }
-
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUsd,"Didnot send enough ETH");
+        require(msg.value.getConversionRate() >= minimumUsd,"Didnot send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender]+ msg.value;
-    }
-
-    function getPrice() public view returns (uint256){
-        // address and ABI
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // Convert price from 8 to 18 decimal places
-        return uint256(price*1e10);
-        
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        // converting to usd
-        uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18;
-        return ethAmountInUsd;
     }
 }
